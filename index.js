93136922501024c4e76f369ac1a96043ea97471e
Andy Gardner <NinjaManatee@users.noreply.github.com>
2025-02-10 10:56:29 -0500

    Updated action (#1)
    * Updated node version and removed required for file-name, since default is specified
    * Updated license
    * Updated versions of actions/core and screenshot-desktop
    * Removed upload of screenshot to artifacts
    * Updated workflow to upload artifact
    * Test with different name

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const screenshot = require("screenshot-desktop");
 const core = require("@actions/core");
-const artifact = require("@actions/artifact");
 
 const fileName = core.getInput("file-name");
 
-const artifactClient = artifact.create();
-
 async function uploadScreenshot() {
 	await screenshot({ filename: fileName });
-	await artifactClient.uploadArtifact(fileName, [fileName], ".");
 }
 
 uploadScreenshot().catch((error) => {
