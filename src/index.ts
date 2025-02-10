006e54f5963af5b088b134df37c3a8a9d22df038
NinjaManatee <andy.gardner@interop.io>
2025-02-10 16:09:20 -0500

    Fixed issue with artifact client Switched to typescript

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import screenshot from "screenshot-desktop";
+import core from "@actions/core";
+import { DefaultArtifactClient } from '@actions/artifact'
+
+const fileName = core.getInput("file-name");
+
+const artifactClient = new DefaultArtifactClient();
+
+async function uploadScreenshot() {
+	await screenshot({ filename: fileName });
+	await artifactClient.uploadArtifact(fileName, [fileName], ".");
+}
+
+uploadScreenshot().catch((error) => {
+	core.setFailed(error.message);
+});
